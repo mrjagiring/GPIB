2b36ef1fcd4c135893a15034df3d52fd7ffa9555
victorsiahaan03 <victorsiahaan03@gmail.com>
2015-06-06 11:24:13 +0700

    adding Template Libraries
    /libraries. to /welcome route

---
+++ CI/application/views/templates/view.php
@@ -0,0 +1,371 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>GPIB / Pekanbaru</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url() ;?>public/css/bootstrap.css" rel="stylesheet">
+    
+    <link href="<?php echo base_url() ;?>public/css/main.css" rel="stylesheet">
+    <link href="<?php echo base_url() ;?>public/fonts/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+      <div class="container">
+                
+                <div id="header">
+                    <div class="row">
+                            <div class="col-md-1 paddingless">
+                                    <div class="logo"><img src="<?php echo base_url() ;?>public/images/Logo_GPIB.png" /></div>
+                            </div>
+                            <div class="header-text-first">gpib</div>
+                            <div class="header-text-second">
+                                <div class="top">Immanuel</div>
+                                <div class="bottom">Pekanbaru</div>
+                            </div>
+                            <div class="header-text-red text-right">
+                                <p>Dan orang akan datang dari timur dan barat dan dari utara dan selatan, dan mereka duduk makan di dalam Kerajaan Allah (Lukas 13:29)</p>
+                            </div>
+
+                    </div>
+                </div>
+
+                <!-- Fixed navbar -->
+                <nav class="navbar navdecoration pull-right text-center">
+                  <div class="container">
+                      <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Tentang Kami</a></li>
+                        <li><a href="#">Fungsionaris</a></li>
+                        <li><a href="#">Pelayanan</a></li>
+                        <li><a href="#">Materi/Download</a></li>
+                      </ul>
+                    </div><!--/.nav-collapse -->
+                </nav>
+                
+                <!-- Slider Goes Here -->
+                <div class="row">
+                    <div class="col-lg-12 paddingless">
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators hidden">
+                          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner custom-carousel" role="listbox">
+                          <div class="item active">
+                            <img src="<?php echo base_url() ;?>public/images/sliders/603074_10200797453925873_1917385995_n_wide.jpg" alt="slider 1" class="img-responsive">
+                            <div class="carousel-caption">
+                                <h3>Hadapi Semua Dengan Senyuman</h3>
+                                <p>lorem ipsum dolor amet</p>
+                            </div>
+                          </div>
+                          <div class="item">
+                             <img src="<?php echo base_url() ;?>public/images/sliders/603074_10200797453925873_1917385995_n_wide.jpg" alt="slider 1" class="img-responsive">
+                            <div class="carousel-caption">
+                                <div class="layer-out">
+                                    <h3>Caption 1</h3>
+                                </div>
+                                
+                                <p>lorem ipsum dolor amet</p>
+                            </div>
+                          </div>
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                          <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                          <span class="sr-only">Next</span>
+                        </a>
+                      </div>
+                    </div>    
+                </div>
+                
+                <div class="clearfix"></div>
+                
+                <div class="row">
+                    <div class="col-lg-12 marquee">
+                            Selamat ulang tahun kepada Bapak Antoneta Lodia Hutuely yang ke 57, - Selamat Ulang tahun Perkawinan kepada Bapak ANTONETA LODIA HUTUELY & Ibu Sthepanny Akyuwen
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="col-lg-3 paddingless news">
+                    <h4>CATATAN DARI MEJA PENDETA</h4>
+                        <div class="border-red"></div>
+                        <div class="border-grey"></div>
+
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">memaknai peristiwa pentakosta </a></div>
+                            </div>
+                            
+                        </div>
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">barang siapa takut ia tidak sempurna </a></div>
+                            </div>
+                            
+                        </div>
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">memaknai peristiwa pentakosta </a></div>
+                            </div>
+                            
+                        </div>
+
+              
+                    <div class="clearfix"></div>    
+                    <h4>BERITA MAJELIS SINODE GPIB</h4>
+                        <div class="border-red"></div>
+                        <div class="border-grey"></div>
+                        
+                        
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">memaknai peristiwa pentakosta </a></div>
+                            </div>
+                            
+                        </div>
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">barang siapa takut ia tidak sempurna </a></div>
+                            </div>
+                            
+                        </div>
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">memaknai peristiwa pentakosta </a></div>
+                            </div>
+                            
+                        </div>
+                        
+                        <div class="clearfix"></div>
+                        <h4>INFORMASI SEKRETARIAT</h4>
+                        <div class="border-red"></div>
+                        <div class="border-grey"></div>
+                        
+                        
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">memaknai peristiwa pentakosta </a></div>
+                            </div>
+                            
+                        </div>
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">barang siapa takut ia tidak sempurna </a></div>
+                            </div>
+                            
+                        </div>
+                        <div class="news-item">
+                            <img src="http://placehold.it/120x80" />
+                            <div class="news-item-text">
+                                <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                <div class="news-item-text caption"><a href="#">memaknai peristiwa pentakosta </a></div>
+                            </div>
+                            
+                        </div>
+                        
+
+                </div>
+                <div class="col-lg-6 main-news">
+                    
+                     <?php $this->load->view($content_template) ;?>
+                    
+                </div>
+                
+                <div class="col-md-3">
+                    <form class="navbar-form" role="search">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                        <div class="input-group-btn">
+                            <button class="btn btn-danger" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                        </div>
+                    </div>
+                    </form>
+                     <h4>SELAMAT ULANG TAHUN</h4>
+                        <div class="border-red"></div>
+                        <div class="border-grey"></div>
+                        <div class="clearfix"></div>
+                        <div class="right-event">
+                            <ul>
+                                <li><a href="#">Bp. Antoneta LODIA HOTUELY</a></li>
+                                <li><a href="#">Sdri Sthepanny Akyuwen</a></li>
+                                <li><a href="#">Ibu Dkn MEIKE SONDAKH MAMBU</a></li>
+                                <li><a href="#">Sdr MATHIAS MATEOS</a></li>
+                            </ul>
+                        </div>
+                        
+                        <h4>SELAMAT ULANG TAHUN PERKAWINAN</h4>
+                        <div class="border-red"></div>
+                        <div class="border-grey"></div>
+                        <div class="clearfix"></div>
+                        <div class="right-event">
+                            <ul>
+                                <li><a href="#">Bp. Antoneta LODIA HOTUELY & Ibu Dkn MEIKE SONDAKH MAMBU</a></li>
+                                <li><a href="#">Bp. MATHIAS MATEOS & Ibu STHEPANNY AKYUWEN</a></li>
+                            </ul>
+                        </div>
+                        
+                        
+                        <h4>BERITA GPIB IMMANUEL PEKANBARU</h4>
+                        <div class="border-red"></div>
+                        <div class="border-grey"></div>
+                        <div class="clearfix"></div>
+                        
+                        <div class="news">
+                                <div class="news-item">
+                                    <img src="http://placehold.it/100x80" />
+                                    <div class="news-item-text">
+                                        <div class="news-item-text date">Selasa, 26 Mei 2015</div>
+                                        <div class="news-item-text caption"><a href="#">Pemberkatan Perkawinan Kudus - Sabtu, 6 Juni 2015</a></div>
+                                    </div>
+
+                                </div>
+                        </div>
+                        
+                        <div class="clearfix"></div>
+                        
+                        <div class="right-event">
+                            <div class="title">Selasa, 26 Mei 2015</div>
+                            <a>LOWONGAN PEKERJAAN Dibutuhkan Tenaga Mechanical Enginering</a>
+                        </div>
+                        
+                         <div class="right-event">
+                            <div class="title">Selasa, 26 Mei 2015</div>
+                            <a>SEMINAR PERSEPULUHAN dan MEMBERI DENGAN IMAN by Pdt H Ongirwalu</a>
+                        </div>
+                        
+                        <div class="clearfix"></div>
+                        
+                        <h4>JEMAAT SAKIT DAN MOHON DUKUNGAN DOA</h4>
+                        <div class="border-red"></div>
+                        <div class="border-grey"></div>
+                        <div class="clearfix"></div>
+                        
+                        <div class="last-event">
+                            <div class="title">Selasa, 26 Mei 2015</div>
+                            <a>Jemaat yang Sakit Minggu, 10 Mei 2015</a>
+                        </div>
+                        
+                        <div class="last-event">
+                            <div class="title">Selasa, 26 Mei 2015</div>
+                            <a>Jemaat yang Sakit Minggu, 10 Mei 2015</a>
+                        </div>
+                        
+                        <div class="last-event">
+                            <div class="title">Selasa, 26 Mei 2015</div>
+                            <a>Jemaat yang Sakit Minggu, 10 Mei 2015</a>
+                        </div>  
+                    
+                </div>
+                <div class="clearfix"></div>
+                
+                <div class="col-lg-12 paddingless">
+                    <div class="col-lg-4 bottom-event">
+                        <h3>kegiatan penting minggu ini</h3>
+
+                                <ul>
+                                    <li><a href="#">Lorem ipsum dolor</a></li>
+                                    <li><a href="#">Conse ctetur adipisicing</a></li>
+                                    <li><a href="#">Reprehenderit voluptate</a></li>
+                                    <li><a href="#">Eismod Tempor</a></li>
+                                    <li><a href="#">Magna Aliqua</a></li>
+                                    <li><a href="#">Cupidatat non proident</a></li>
+                                </ul> 
+
+                    </div>
+                    <div class="col-lg-4 bottom-event">
+                            <h3 class="text-center">graph kehadiran jemaat</h3>
+                    </div>
+                    
+                    <div class="col-lg-4 bottom-contact">
+                            <div class="header">GPIB Immanuel</div>
+                            <div class="subheader">Pekanbaru</div>
+                            <div class="address">Jl.Sumatera No. 21-23 Pekanbaru, Riau 2816 <br />Email: gpibimmanuelpekanbaru@gmail.com</div>
+                            <div class="phone">Tel: 0761-21178; 0761-47893</div>
+                    </div>
+                </div>
+                
+                <div class="clearfix"></div>
+                <div class="row">
+                        
+                    <hr>
+                        
+                </div>
+                
+                <div class="social-icon">
+                        <i class="fa fa-pinterest-p fa-3x"></i>
+                        <i class="fa fa-skype fa-3x"></i>
+                        <i class="fa fa-linkedin fa-3x"></i>
+                        <i class="fa fa-facebook fa-3x"></i>
+                        <i class="fa fa-twitter fa-3x"></i>
+                        
+                        
+                        
+                             
+                </div>
+          
+      </div>
+      
+     
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url() ;?>public/js/jquery-1.11.2.min.js"></script>
+    <script src="<?php echo base_url() ;?>public/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ;?>public/js/jquery.marquee.js"></script>
+    <script src="<?php echo base_url() ;?>public/js/jquery.pause.js"></script>
+    <script>
+	$(function(){
+		$('.marquee').marquee({
+                        speed: 10000,
+                        gap: 50,
+                        delayBeforeStart: 0,
+                        direction: 'left',
+                        pauseOnHover: true
+                });		
+	});
+    </script>
+
+    
+  </body>
+</html>
